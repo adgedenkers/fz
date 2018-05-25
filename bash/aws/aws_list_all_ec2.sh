67eb31f12beeb4dcd66dec8a1453676d5e3f0d1f
adgedenkers <adge@saturn.frontierlocal.net>
2018-05-25 08:39:15 -0400

    small script changes

--- bash/aws/aws_list_all_ec2.sh
+++ bash/aws/aws_list_all_ec2.sh
@@ -15,14 +15,23 @@
 # VHA Future Technology Laboratory
 # U.S. Department of Veterans Affairs
 # project: FTL Tools
-# file name: list_all_systems.sh
-# location: /Users/adge/bin/aws
+# file name: aws_list_all_ec2.sh
+# location: /Users/adge/bin/fz/bash/aws/
 # date: 2018-01-23
+# updated: 2018-05-22
 # -------------------------------------------------------
 
-# ----- Output New Header Row Labels to the TSV file `FTL_Systems.tsv`
+currTs=$(date +"%Y-%m-%d_%H-%M-%S")
+tsv_file_path="/Users/adge/bin/fz/bash/aws/reports"
+tsv_file_name="$currTs-FTL-EC2-Report.tsv"
+
+tsv_file="$tsv_file_path/$tsv_file_name"
+
+touch $tsv_file
+
+# ----- Output New Header Row Labels to the TSV file 
 reportHead="Instance ID\tProject ID\tProject Name\tName\tInstance Type\tState\tKey\tOS\tDNS\tPOC\tEmail\tImage ID\tPublic DNS\tPublic IP\tLaunched"
-echo -e $reportHead > all_systems.tsv
+echo -e $reportHead > "$tsv_file"
 
 # ----- Query AWS EC2 for all FTL Instances - Added Additional Fields
-aws ec2 describe-instances --output text --query 'Reservations[*].Instances[*].[InstanceId, [Tags[?Key==`Project ID`].Value][0][0], [Tags[?Key==`Project Name`].Value][0][0],[Tags[?Key==`Name`].Value][0][0],InstanceType,State.Name,KeyName,[Tags[?Key==`OS`].Value][0][0],[Tags[?Key==`DNS`].Value][0][0],[Tags[?Key==`POC`].Value][0][0],[Tags[?Key==`Email`].Value][0][0],ImageId,PublicDnsName,PublicIpAddress,LaunchTime ]' >> all_systems.tsv
+aws ec2 describe-instances --output text --query 'Reservations[*].Instances[*].[InstanceId, [Tags[?Key==`Project ID`].Value][0][0], [Tags[?Key==`Project Name`].Value][0][0],[Tags[?Key==`Name`].Value][0][0],InstanceType,State.Name,KeyName,[Tags[?Key==`OS`].Value][0][0],[Tags[?Key==`DNS`].Value][0][0],[Tags[?Key==`POC`].Value][0][0],[Tags[?Key==`Email`].Value][0][0],ImageId,PublicDnsName,PublicIpAddress,LaunchTime ]' >> "$tsv_file"
